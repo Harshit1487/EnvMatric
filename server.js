008f69a845de649b6a2bc4033d560a0557df3e0d
Harshit Raj <rharshit82@gmail.com>
2021-09-26 13:15:19 +0530

    deploying

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ dbConnect();
 //Basic Routes
 app.use('/api', apiRoutes)
 
+const path = require("path");
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 const PORT = process.env.PORT
 app.listen(PORT || 5000, () => console.log('Server Running'))
